a7647655e19f67e7f141a1c9c00bd219dfcc8814
VladislavTurukin <tvvmobile@gmail.com>
2024-06-24 01:31:00 +0300

    Init Commit

---
+++ src/main/java/site/notion/systemeio/systemeio_app/SystemeioAppApplication.java
@@ -0,0 +1,13 @@
+package site.notion.systemeio.systemeio_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeioAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemeioAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/notion/systemeio/systemeio_app/SystemeioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package site.notion.systemeio.systemeio_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
